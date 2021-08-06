b346f9b5de21a45627b4c28d366f2b9023a9b003
sumitdahiya619 <naughtysumit619@gmail.com>
2021-08-06 22:38:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'package:flash_chat_flutter/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print('error');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            initialRoute: WelcomeScreen.id,
+            routes: {
+              WelcomeScreen.id: (context) => WelcomeScreen(),
+              ChatScreen.id: (context) => ChatScreen(),
+              RegistrationScreen.id: (context) => RegistrationScreen(),
+              LoginScreen.id: (context) => LoginScreen(),
+            },
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}
